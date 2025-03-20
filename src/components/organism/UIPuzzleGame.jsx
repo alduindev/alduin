3b089e45e9fd3cd0886e18c3537255cd99fd8a15
Alduin <urielcabadat@hotmail.com>
2025-03-20 10:23:00 -0500

    UpdateButtonDarkMode

--- src/components/organism/UIPuzzleGame.jsx
+++ src/components/organism/UIPuzzleGame.jsx
@@ -476,7 +476,7 @@ const UIPuzzleGame = () => {
         className="px-4 py-2 bg-gray-600 text-white rounded-lg"
         onClick={toggleDarkMode}
       >
-        {isDarkMode ? "Modo Día ☀️" : "Modo Noche 🌙"}
+        {isDarkMode ? "☀️" : "🌙"}
       </button>
       <p className="text-gray-300 mt-4">V.1.0.2 - Creado con amor</p>
     </div>
