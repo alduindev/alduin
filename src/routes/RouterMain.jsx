6808af9d2b2f6540ed2f1a3e3fdf4faccf9215f1
Alduindev <urielcabadat@hotmail.com>
2026-03-12 18:32:11 -0500

    Fix3

--- src/routes/RouterMain.jsx
+++ src/routes/RouterMain.jsx
@@ -8,7 +8,6 @@ import PageBreakout from "../pages/PageBreakout";
 import PageTesting from "../pages/PageTesting";
 import PagePaint from "../pages/PagePaint";
 import PageMatchstick from "../pages/PageMatchstick";
-import PageEmulator from "../pages/PageEmulator";
 import PageWin from "../pages/PageWin";
 import PageRuleta3D from "../pages/PageRuleta3D";
 import PageCube from "../pages/PageCube";
@@ -21,7 +20,6 @@ export default function RouterMain() {
         <Route path="/music" element={<PageMusic />} />
         <Route path="/puzzle" element={<PagePuzzle />} />
         <Route path="/matchstick" element={<PageMatchstick />} />
-        {/* <Route path="/emulator" element={<PageEmulator />} /> */}
         <Route path="/windows" element={<PageWin />} />
         <Route path="/paint" element={<PagePaint />} />
         <Route path="/website" element={<PageWebsite />} />
